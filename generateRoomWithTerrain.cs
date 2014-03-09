cb4db1175db3154b5a43e2ef46a6f2ac347bf981
skylarjhdownes <skylarjhdownes@gmail.com>
2014-03-09 00:56:32 -0700

    Adding back in my files.

---
+++ generateRoomWithTerrain.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class generateRoomWithTerrain : MonoBehaviour {
+
+	public GameObject generatedTerrain;
+	public Vector3 roomDimensions, roomPosition;
+
+	// Use this for initialization
+	generateRoomWithTerrain (Vector3 terrainSize_in, Vector3 roomPosition_in, ) {
+		GameObject terrain = GameObject.CreatePrimitive(PrimitiveType.Plane);
+		GameObject ceiling = GameObject.CreatePrimitive(PrimitiveType.Plane);
+		generatedTerrain.transform.position = roomPosition_in;
+		generatedTerrain.transform.localScale = terrainSize_in;
+	}
+
+	void setRoomDimensions(Vector3 terrainSize_in) {
+		roomDimensions = terrainSize_in;
+		generatedTerrain.transform.localScale = terrainSize_in;
+	}
+
+	void setRoomPosition(Vector3 roomPosition_in) {
+		roomPosition = roomPosition_in;
+		generatedTerrain.transform.position = roomPosition_in;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
